311624ac0e2f8d74b652a5673ee403bdd9335868
Yunwei-Zhang <62420557+Yunwei-Zhang@users.noreply.github.com>
2021-05-11 14:36:08 +0800

    updates p2
    make it more clear

--- Assets/Scripts/MessageP2End.cs
+++ Assets/Scripts/MessageP2End.cs
@@ -13,11 +13,11 @@ public class MessageP2End : MonoBehaviour
         var dialogTexts = new List<DialogData>();
 
         dialogTexts.Add(new DialogData("You return to records and lay the bolts of fabric at the girl’s feet.", "Narrator"));
-        dialogTexts.Add(new DialogData("I’m sorry you had to put yourself at risk for me, when my predicament is my own fault.", "Younggirl"));
-        dialogTexts.Add(new DialogData("You see, I ran out of firewood, but I could not bear to use the books as kindling.", "Younggirl"));
-        dialogTexts.Add(new DialogData("I have good news, though. According to the library records, a map of the manor was checked out by Dr. Edwards, and his office is only one floor above.", "Younggirl"));
+        dialogTexts.Add(new DialogData("I’m sorry you had to put yourself at risk for me, when\nmy predicament is my own fault.", "Younggirl"));
+        dialogTexts.Add(new DialogData("You see, I ran out of firewood, but I could not bear to\nuse the books as kindling.", "Younggirl"));
+        dialogTexts.Add(new DialogData("I have good news, though. According to the library\nrecords, a map of the manor was checked out by\nDr. Edwards, and his office is only one floor above.", "Younggirl"));
         dialogTexts.Add(new DialogData("You should come with me. It’s not safe here.", "Protagonist"));
-        dialogTexts.Add(new DialogData("You are a compassionate soul, I can tell. Thank you, but safe or not…", "Younggirl"));
+        dialogTexts.Add(new DialogData("You are a compassionate soul, I can tell. Thank you,\nbut safe or not…", "Younggirl"));
         DialogData finalLine = new DialogData("I cannot leave the library.", "Younggirl");
 
         DialogManager.Show(dialogTexts);
