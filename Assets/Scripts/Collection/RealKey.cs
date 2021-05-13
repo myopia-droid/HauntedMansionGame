d5da944a493e54a75b48a783e9ea8b1b9d80e540
Yunwei-Zhang <62420557+Yunwei-Zhang@users.noreply.github.com>
2021-05-13 20:02:24 +0800

    Merge branch 'main' of https://github.com/myopia-droid/HauntedMansionGame into main

--- Assets/Scripts/Collection/RealKey.cs
+++ Assets/Scripts/Collection/RealKey.cs
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,39 +14,3 @@ public class RealKey : MonoBehaviour
         }
     }
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class RealKey : MonoBehaviour
-{
-    public GameObject key;
-    
-    void OnTriggerEnter2D(Collider2D other){
-        if(other.gameObject.CompareTag("Player"))
-        {
-          
-          Destroy(key);       
-        }
-    }
-}
->>>>>>> ed4648a097820aeb8aa537bfa568111cdb6bb006
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class RealKey : MonoBehaviour
-{
-    public GameObject key;
-    
-    void OnTriggerEnter2D(Collider2D other){
-        if(other.gameObject.CompareTag("Player"))
-        {
-          
-          Destroy(key);       
-        }
-    }
-}
->>>>>>> ed4648a097820aeb8aa537bfa568111cdb6bb006
