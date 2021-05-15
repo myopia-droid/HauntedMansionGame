37f1e06a1a009ab426a11d69a517a5791b0580b8
myopia-droid <54422430+myopia-droid@users.noreply.github.com>
2021-05-15 14:56:15 +1000

    Boss Level
    - Swipe animation
    - Set up animator controller to idle 3.5 seconds, repeat
    - Set up scene with platforms and decorations
    - Created swipeLeft and idleLeft animations in order to later set up animator controller so idle -> swipe -> idleLeft -> swipeLeft

--- Assets/Scripts/enemy/Boss/FinalBoss/idleFinal.cs
+++ Assets/Scripts/enemy/Boss/FinalBoss/idleFinal.cs
@@ -5,16 +5,13 @@ using UnityEngine;
 public class idleFinal : StateMachineBehaviour
 {
     public float timer;
-    private Transform playerPos;
     private GameObject player;
 
-    private bool leftFacing;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         timer = 3.5f;
-        leftFacing = false;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -23,31 +20,10 @@ public class idleFinal : StateMachineBehaviour
         //if player is not dead
         if (GameObject.FindGameObjectWithTag("Player") != null)
         {
-            Vector3 Scaler = animator.transform.localScale;
-
-            //flips enemy sprite to current state
-            if (leftFacing)
-            {
-                Scaler.x = -1;
-            }
-            else
-            {
-                Scaler.x = 1;
-            }
-
-            animator.transform.localScale = Scaler;
-
             //If time is up, swipe and switch sides for next time boss is idle. Otherwise continue countdown.
             if (timer <= 0)
             {
                 animator.SetTrigger("swipe");
-                if (leftFacing)
-                {
-                    leftFacing = false;
-                } else
-                {
-                    leftFacing = true;
-                }
             }
             else
             {
--- Assets/Scripts/enemy/Boss/FinalBoss/swipeBehaviour.cs
+++ Assets/Scripts/enemy/Boss/FinalBoss/swipeBehaviour.cs
@@ -4,31 +4,34 @@ using UnityEngine;
 
 public class swipeBehaviour : StateMachineBehaviour
 {
- 
+ //SWIPER NO SWIPING
 
     public GameObject player;
+    public float timer;
+
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-    
+        timer = animator.GetCurrentAnimatorClipInfo(0)[0].clip.length;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       
+        if (timer <= 0.0)
+        {
+            animator.SetTrigger("idle");
+        } else
+        {
+            timer -= Time.deltaTime;
+        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        //DAMAGE PLAYER IF ON THE SIDE OF THE SCREEN SWIPED AT
 
-        if (animator.transform.localScale.x == -1) {
-            //damage player if on left side of screen
-        } else if (animator.transform.localScale.x == 1)
-        {
-            //damage player if on right side of screen
-        }
     }
 }
